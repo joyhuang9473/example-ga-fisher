3907b7a5f2bbbbf7c1d07c62005e331dffbdb515
irisshu <irishsuyt@gmail.com>
2014-11-28 11:05:12 +0800

    Detect, crop, resize the "face".

--- FaceDetection.m
+++ FaceDetection.m
@@ -4,10 +4,17 @@
 FDetect = vision.CascadeObjectDetector;
 
 %Read the input image
-I = imread('29.bmp');
+I = imread('26.bmp');
 
 %Returns Bounding Box values based on number of objects
 BB = step(FDetect,I);
+% step(Detector,I) returns Bounding Box value that contains [x,y,Height,Width].
+cap_face = imcrop(I,BB);
+cap_face = imresize(cap_face, [250 250])
+
+figure,
+imshow(cap_face);
+
 
 figure,
 imshow(I); hold on
